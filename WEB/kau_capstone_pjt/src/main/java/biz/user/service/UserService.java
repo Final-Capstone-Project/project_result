1732e68814b60784454d7c03230d0ffa5fa73e06
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-09-21 21:27:48 +0900

    Update UserService.java

--- WEB/kau_capstone_pjt/src/main/java/biz/user/service/UserService.java
+++ WEB/kau_capstone_pjt/src/main/java/biz/user/service/UserService.java
@@ -6,6 +6,7 @@ import biz.user.vo.UserVO;
 public interface UserService {
 
 	UserVO login(String id, String pw);
+	UserVO getUser(String id);
 	int addUser(UserVO user) throws Exception;
 	int updateUser(UserVO user);
 	int removeUser(String userid);
