85be5f43d84d37770d5deb16e1b52f0bf048ce5a
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-10-05 20:56:22 +0900

    Create BoardServiceImpl.java

---
+++ WEB/kau_capstone_pjt/src/main/java/biz/board/service/BoardServiceImpl.java
@@ -0,0 +1,46 @@
+package biz.board.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import biz.board.dao.BoardDAO;
+import biz.board.vo.BoardVO;
+
+@Service("boardservice")
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	@Qualifier("spring")
+	BoardDAO dao;
+	
+	public BoardServiceImpl() {
+		super();
+	}
+	
+	public BoardServiceImpl(BoardDAO dao) {
+		super();
+		this.dao = dao;
+	}
+	
+	@Override
+	public BoardVO getBoard(int board_id) {
+		return dao.getBoard(board_id);
+	}
+
+	@Override
+	public int addBoard(BoardVO board) {
+		return dao.addBoard(board);
+	}
+
+	@Override
+	public int updateBoard(BoardVO board) {
+		return dao.updateBoard(board);
+	}
+
+	@Override
+	public int removeBoard(int board_id) {
+		return dao.removeBoard(board_id);
+	}
+	
+}
