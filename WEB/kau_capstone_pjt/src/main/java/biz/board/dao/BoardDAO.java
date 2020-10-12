8d8eefe45a9f0744af712a1df87fccee598d83cf
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-10-12 21:47:25 +0900

    Update BoardDAO.java

--- WEB/kau_capstone_pjt/src/main/java/biz/board/dao/BoardDAO.java
+++ WEB/kau_capstone_pjt/src/main/java/biz/board/dao/BoardDAO.java
@@ -3,6 +3,7 @@ package biz.board.dao;
 import biz.board.vo.BoardVO;
 
 public interface BoardDAO {
+	List<BoardVO> getBoardList();
 	BoardVO getBoard(int board_id);
 	int addBoard(BoardVO board);
 	int updateBoard(BoardVO board);
