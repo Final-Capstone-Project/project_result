8893b78da4297a14eb8e46a0cafa6ba19d2b794a
LimHyungang <47623969+LimHyungang@users.noreply.github.com>
2020-10-12 21:48:31 +0900

    Update BoardService.java

--- WEB/kau_capstone_pjt/src/main/java/biz/board/service/BoardService.java
+++ WEB/kau_capstone_pjt/src/main/java/biz/board/service/BoardService.java
@@ -3,7 +3,7 @@ package biz.board.service;
 import biz.board.vo.BoardVO;
 
 public interface BoardService {
-	
+	List<BoardVO> getBoardList();
 	BoardVO getBoard(int board_id);
 	int addBoard(BoardVO board);
 	int updateBoard(BoardVO board);
